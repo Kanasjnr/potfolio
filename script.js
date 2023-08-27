3344e83f413a1561184e2b5b80649a5e83d6ad39
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-08-27 11:50:38 +0100

    git update

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function toggleMenu() {
-    const menuLinks = document.querySelector(".menu-links");
-    menuLinks.classList.toggle("active");
-}
+const btnMobileEl = document.querySelector(".btn-mobile-nav");
+const navEl = document.querySelector(".navigationBar");
+
+btnMobileEl.addEventListener("click", function() {
+    navEl.classList.toggle("nav-open")
+});
